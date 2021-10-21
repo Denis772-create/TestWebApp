51c26abd6adff513f0a9e3c7cc17e6f4105512ba
Denis <denis_aleksandrov_12@mail.ru>
2021-10-21 21:50:37 +0300

    s

--- src/TestWebApp.DAL/Data/Migrations/20211021181137_qop.Designer.cs
+++ src/TestWebApp.DAL/Data/Migrations/20211021181137_qop.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TestWebApp.DAL.Data;
 
 namespace TestWebApp.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211021181137_qop")]
+    partial class qop
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/TestWebApp.DAL/Data/Migrations/20211021181137_qop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestWebApp.DAL.Migrations
+{
+    public partial class qop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Orders",
+                newName: "DateOfCreation");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOfCreation",
+                table: "Orders",
+                newName: "UserId");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Orders",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
