21212d4d5ad37743867782b343813b6489e6fb3e
Ruslan <ruslan>
2021-10-21 22:23:01 +0300

    udpate: order model

--- src/TestWebApp.DAL/Models/Entities/Order.cs
+++ src/TestWebApp.DAL/Models/Entities/Order.cs
@@ -1,9 +1,13 @@
-﻿
+﻿using System.Collections.Generic;
+
 namespace TestWebApp.DAL.Models.Entities
 {
     public class Order : BaseEntity
     {
         public string UserId { get; set; }
         public virtual User User { get; set; }
+
+        public string ProductId { get; set; }
+        public ICollection<Product> Products { get; set; }
     }
 }
