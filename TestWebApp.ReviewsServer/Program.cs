504616acb5c11bccc1ef83c702cdf590017e61ab
Denis ALeksandrov <denis_aleksandrov_12@mail.ru>
2021-10-31 16:15:32 +0300

    Update: edit name TestWebApp.ReviewsServer on TestWebApp.ReviewServer

--- TestWebApp.ReviewsServer/Program.cs
+++ TestWebApp.ReviewsServer/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TestWebApp.ReviewsServer
+namespace TestWebApp.ReviewServer
 {
     public class Program
     {
--- TestWebApp.ReviewsServer/Startup.cs
+++ TestWebApp.ReviewsServer/Startup.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TestWebApp.ReviewsServer
+namespace TestWebApp.ReviewServer
 {
     public class Startup
     {
